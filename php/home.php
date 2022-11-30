1baea511c53a415c84a84ed15e0beb00d541f7e2
Rifle1 <philipe.barakat@yahoo.com>
2022-11-30 22:02:54 +0100

    updt

---
+++ php/home.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/home-styles.css">
+    <link rel="stylesheet" href="../css/global-styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap" rel="stylesheet">
+    <title>PetConnect</title>
+</head>
+<body>
+<div class="main-header">
+
+    <div class="logo">
+        <img src="../img/logos/PetConnect-Logo.png" alt="PetConnect-Logo">
+    </div>
+
+    <div class="header-font" id="menu">
+        <a href="home.php">Accueil</a>
+        <a href="#">Boutique</a>
+        <a href="#">Assistance</a>
+    </div>
+
+    <div class="profile">
+        <div id="profile-div-flex">
+            <div id="profile-logo"><a href="#"><img id="profile-img-1" src="../img/profile/client.png" alt="Client-logo"></a></div>
+            <div id="dropdown-menu" class="text-font">
+                <p>Bonjour XXXXX</p>
+                <div class="dropdown-menu-line"></div>
+                <a href="#">Mon Compte</a>
+                <a href="#">Mes Commandes</a>
+                <a href="#">Mes Appareils</a>
+                <div class="dropdown-menu-line"></div>
+                <a href="#">Logout</a>
+            </div>
+        </div>
+        <div>
+            <a href="#"><img id="profile-img-2" src="../img/profile/basket.png" alt="Basket-logo"></a>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="background-dog-div">
+    <img id="background-img" src="../img/dogs/chien.jpg" alt="Main-Dog">
+    <img id="foreground-img" src="../img/logos/iCollar_logo.png" alt="iCollar_logo">
+</div>
+
+<div class="header-font" id="slogan">
+    <h1>La technologie pour vos animaux</h1>
+</div>
+
+<div>
+    <?php
+    $name = "aa";
+    echo "$name";
+    ?>
+</div>
+
+<div>
+    <?php
+
+    ?>
+</div>
+
+</body>
+</html>
