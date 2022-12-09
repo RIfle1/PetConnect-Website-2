c1fdebb5bd61c21906f8421381461d18a41f2957
Filips Barakats <philipe.barakat@yahoo.com>
2022-12-09 14:54:45 +0100

    updt

--- php/manage-client.php
+++ php/manage-client.php
@@ -20,6 +20,7 @@ if(!empty($_SESSION['admID'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/manage-client-styles.css">
+    <script src="../javaScript/manage-client-buttons.js"></script>
     <title>manage-client</title>
 </head>
 <body>
