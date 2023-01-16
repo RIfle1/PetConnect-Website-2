51a94225a91d2db2c7d713684756f21d9c4c9125
gentleman213 <100601616+gentleman213@users.noreply.github.com>
2023-01-16 11:56:09 +0100

    nul
    sasa

--- php-processes/signup-process.php
+++ php-processes/signup-process.php
@@ -6,15 +6,14 @@ include 'verification-functions.php';
 
 $languagesList = returnLanguageList()[returnLanguage()]['signup-process'];
 
-$newUserInfo = new Client
-(
-    $_POST["cltUsername-input"],
-    $_POST["cltFirstName-input"],
-    $_POST["cltLastName-input"],
-    $_POST["cltEmail-input"],
-    $_POST["cltPhoneNumber-input"],
-    $_POST["cltPassword-input"]
-);
+$newUserInfo = new Client(
+        $_POST["cltUsername-input"],
+        $_POST["cltFirstName-input"],
+        $_POST["cltLastName-input"],
+        $_POST["cltEmail-input"],
+        $_POST["cltPhoneNumber-input"],
+        $_POST["cltPassword-input"]
+    );
 
 $cltID = $newUserInfo->getCltID();
 $cltUsername = $newUserInfo->getCltUsername();
@@ -25,23 +24,23 @@ $cltPhoneNumber = $newUserInfo->getCltPhoneNumber();
 $cltPassword = $newUserInfo->getCltPassword();
 $cltToken = $newUserInfo->getCltToken();
 
-if(empty($cltUsername)) {
+if (empty($cltUsername)) {
     die("Username Is Required");
 }
 
-if(empty($cltFirstName)) {
+if (empty($cltFirstName)) {
     die("First Name Is Required");
 }
 
-if(empty($cltLastName)) {
+if (empty($cltLastName)) {
     die("Last Name Is Required");
 }
 
-if (! filter_var($cltPhoneNumber, FILTER_VALIDATE_INT)) {
+if (!filter_var($cltPhoneNumber, FILTER_VALIDATE_INT)) {
     die("Valid Phone Number is Required");
 }
 
-if (! filter_var($cltEmail, FILTER_VALIDATE_EMAIL)) {
+if (!filter_var($cltEmail, FILTER_VALIDATE_EMAIL)) {
     die("Valid Email is Required");
 }
 
@@ -53,7 +52,7 @@ checkPasswordLetter($cltPassword);
 checkPasswordNumber($cltPassword);
 checkPasswordMatch($cltPassword, $newPasswordConfirmation);
 
-$emailSQL = "SELECT cltEmail FROM Client WHERE cltEmail='".$cltEmail."'";
+$emailSQL = "SELECT cltEmail FROM Client WHERE cltEmail='" . $cltEmail . "'";
 $result = runSQLResult($emailSQL);
 $userEmail = $result->fetch_assoc();
 
@@ -69,13 +68,13 @@ $verificationCode = generateVerificationCode();
 $body = returnEmailCodeValidationStructure($verificationCode)["body"];
 $subject = returnEmailCodeValidationStructure($verificationCode)["subject"];
 
-if(sendGmail($cltEmail, $cltFirstName, $body, $subject)) {
+if (sendGmail($cltEmail, $cltFirstName, $body, $subject)) {
     $cltPasswordHash = returnPasswordHash($cltPassword);
 
     $insertSQL = "INSERT INTO client (`cltID`, `cltUsername`, `cltFirstName`, `cltLastName`, `cltEmail`, `cltPhoneNumber`, `cltPassword`, `cltIsModerator`, `cltVerifiedEmail`, cltToken)
-        VALUES ('".$cltID."', '".$cltUsername."', '".$cltFirstName."', '".$cltLastName."', '".$cltEmail."', '".$cltPhoneNumber."', '".$cltPasswordHash."', 0, 0,'".$cltToken."')";
+        VALUES ('" . $cltID . "', '" . $cltUsername . "', '" . $cltFirstName . "', '" . $cltLastName . "', '" . $cltEmail . "', '" . $cltPhoneNumber . "', '" . $cltPasswordHash . "', 0, 0,'" . $cltToken . "')";
 
-    if(insertSQL($insertSQL) == "Success") {
+    if (insertSQL($insertSQL) == "Success") {
         session_start();
         // Variables in the session
         $_SESSION['verificationCode'] = $verificationCode;
@@ -86,8 +85,7 @@ if(sendGmail($cltEmail, $cltFirstName, $body, $subject)) {
 
         header("Location: ../php-pages/signup-success.php", true, 303);
         exit;
-    }
-    else {
+    } else {
         echo insertSQL($insertSQL);
     }
 }
